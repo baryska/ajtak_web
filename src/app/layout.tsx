2754f6fb9fddb116fddf36e4a396ece86592f03a
baryska <barbora.skalova@newtontech.cz>
2025-02-09 21:44:33 +0100

    fix: update layout structure to use <body> tag and prevent overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,10 +103,9 @@ export default function RootLayout({
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Devonshire&family=Ms+Madi&family=Oooh+Baby&family=Playwrite+CZ:wght@100..400&family=Fuzzy+Bubbles:wght@400;700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Kavivanar&family=Special+Elite&family=Yomogi&family=Patrick+Hand+SC&family=Quicksand:wght@300..700&family=Alfa+Slab+One&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
 			</head>
-			<Flex
-				as="body">
+			<body style={{overflow: 'hidden'}}>
 				{children}
-			</Flex>
+			</body>
 		</Flex>
 	);
 }
